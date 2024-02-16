ecd1fbcb285adfc2af9d49bad7815cd30c097ee6
ascaryaaa <allaamascarya@gmail.com>
2024-02-16 08:53:31 +0700

    adding routes for anime and manga

--- src/app/anime/anime.module.ts
+++ src/app/anime/anime.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RoutingModule } from './routing.module';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    RoutingModule
   ]
 })
 export class AnimeModule { }
