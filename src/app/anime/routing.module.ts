6b9bfc5d78686f2628a3741ef0ebee4335b3510e
ascaryaaa <allaamascarya@gmail.com>
2024-02-16 09:05:17 +0700

    adding path and component in routing module

--- src/app/anime/routing.module.ts
+++ src/app/anime/routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes  } from '@angular/router';
+import { AnimeListComponent } from './anime-list/anime-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'list',
+    component: AnimeListComponent
+  }
+];
 
 @NgModule({
   exports: [RouterModule],
--- src/app/manga/routing.module.ts
+++ src/app/manga/routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes  } from '@angular/router';
+import { MangaListComponent } from './manga-list/manga-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'list',
+    component: MangaListComponent
+  }
+];
 
 @NgModule({
   exports: [RouterModule],
