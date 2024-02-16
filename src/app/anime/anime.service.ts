bf660b372a31e73606395221b009c51e46882b38
ascaryaaa <allaamascarya@gmail.com>
2024-02-16 10:26:47 +0700

    update anime.service.ts to get top anime list

--- src/app/anime/anime.service.ts
+++ src/app/anime/anime.service.ts
@@ -1,14 +1,22 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Anime, GetAnimeResponse } from './anime';
+import { HttpClient } from '@angular/common/http';
+import { topAnimeListEndpoint } from '../config/api';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AnimeService {
+  
+  constructor(private httpClient: HttpClient) { }
 
-  constructor() { }
-
-  // getTopAnimeList(): Observable<> {
-
-  // }
+  getTopAnimeList(): Observable<Array<Anime>> {
+    return new Observable(observer => {
+      this.httpClient.get<GetAnimeResponse>(topAnimeListEndpoint).subscribe(respone => {
+        observer.next(respone.data);
+        observer.complete();
+      })
+    })
+  }
 }
